3c5bdc086c94c1ea5275f099305f9acdd37759dd
Hugo Lima Romão <hugo8romao@gmail.com>
2023-01-12 13:39:43 -0400

    FEAT: Adding styled-components to compiler options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{ hostname: "vestibulares2022.com.br" }],
+  },
 };
 
 module.exports = nextConfig;
