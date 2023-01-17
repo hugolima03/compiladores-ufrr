6020ecd1b937b6d0628c3acfa9eaec6a08cdb530
Hugo Lima Romão <hugo8romao@gmail.com>
2023-01-17 15:26:37 -0400

    initial tokenizer

---
+++ src/functions/tokenizer/index.js
@@ -0,0 +1,110 @@
+// REGEX
+const spacesRegex = /\s+/g;
+const idRegex = /^[a-zA-Z\_][a-zA-Z\_0-9]*$/;
+const numIRegex = /^[0-9]+$/;
+const numFRegex = /^(([0-9]+(\.[0-9]+))|(\.[0-9]+))$/;
+
+// PATTERNS
+const strPattern = "([\"'])(?:(?=(\\\\?))\\2.)*?\\1";
+
+// LEXEMAS
+const boolLiterals = ["true", "false"];
+const keyWords = ["if", "else", "while", "let"];
+const arithmeticOperators = ["+", "-", "*", "/", "%", "="];
+const groupers = ["(", ")", "[", "]", "{", "}", ",", ";"];
+const primitiveTypes = ["int", "float", "string", "bool"];
+
+function matchLexemeCategory(lexeme) {
+  const categories = {
+    primitiveType: (l) =>
+      l.match(new RegExp("^" + primitiveTypes.join("|") + "$")) != null,
+    keyWord: (l) => l.match(new RegExp("^" + keyWords.join("|") + "$")) != null,
+    boolLiteral: (l) =>
+      l.match(new RegExp("^" + boolLiterals.join("|") + "$")) != null,
+    arithmeticOperators: (l) =>
+      l.match(new RegExp("^\\" + arithmeticOperators.join("|\\") + "$")) !=
+      null,
+    grouper: (l) =>
+      l.match(new RegExp("^\\" + groupers.join("|\\") + "$")) != null,
+    id: (l) => l.match(idRegex) != null,
+    literalStr: (l) => l.match(new RegExp("^" + strPattern + "$")) != null,
+    numI: (l) => l.match(numIRegex) != null,
+    numF: (l) => l.match(Lexer.numFRegex) != null,
+    undefined: (l) => true,
+  };
+
+  for (let i in categories) {
+    if (!categories.hasOwnProperty(i)) continue;
+    if (!categories[i](lexeme)) continue;
+    return i;
+  }
+}
+
+function splitSpaces(input) {
+  const strRegex = new RegExp(strPattern, "g");
+  const strs = [...input.matchAll(strRegex)];
+  let frags = [];
+  let cursor = 0;
+
+  for (let i in strs) {
+    const s = strs[i];
+    const left = input.substr(cursor, s["index"] - cursor).trim();
+
+    frags = [...frags, ...left.split(spacesRegex), s[0]];
+
+    cursor = s["index"] + s[0].length;
+  }
+
+  if (cursor < input.length) {
+    frags = [...frags, ...input.substr(cursor).trim().split(spacesRegex)];
+  }
+
+  return frags;
+}
+
+function parseLexemes(line) {
+  const separators = [...arithmeticOperators, ...groupers];
+  const regex = new RegExp(
+    "[\\" + separators.join("\\") + "]|" + strPattern,
+    "g"
+  );
+
+  const frags = splitSpaces(line);
+  const lexemes = [];
+
+  for (let i in frags) {
+    const matched = [...frags[i].matchAll(regex)];
+    let cursor = 0;
+
+    for (let j in matched) {
+      const sep = matched[j][0];
+      const left = frags[i].substr(cursor, matched[j]["index"] - cursor);
+
+      if (left.length > 0) lexemes.push(left);
+      lexemes.push(sep);
+
+      cursor = matched[j]["index"] + sep.length;
+    }
+
+    if (cursor < frags[i].length) lexemes.push(frags[i].substr(cursor));
+  }
+
+  return lexemes;
+}
+
+function tokenizer(line) {
+  const lexemes = parseLexemes(line);
+
+  for (let i in lexemes) {
+    const cat = matchLexemeCategory(lexemes[i]);
+
+    if (cat === "undefined") {
+      console.log('Invalid identifier "' + lexemes[i] + '" at line ' + line);
+    }
+
+    console.log(cat, lexemes[i]);
+  }
+}
+
+const line = "x = y + 5";
+tokenizer(line);
