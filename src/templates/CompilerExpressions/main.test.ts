8696c77413e69a14f1d74c75c7e56593e5949aa6
Hugo Lima Romão <hugo8romao@gmail.com>
2023-06-25 09:19:29 -0400

    test: adding jest and generic test

---
+++ src/templates/CompilerExpressions/main.test.ts
@@ -0,0 +1,56 @@
+import Sintatico from "./sintatico/Sintatico";
+import { getReactD3Tree } from "./sintatico/Arvore";
+
+import Semantico from "./semantico/Semantico.mjs";
+
+import Intermediario from "./sintese/Intermediario.mjs";
+
+import Mips from "./sintese/Mips.mjs";
+
+describe('CompilerExpressions', () => {
+  it('Generic test', () => {
+    const sourceCode = `variaveis
+    var: int;
+    hugo: int;
+inicio
+    var = 0;
+    hugo = 10;
+    retorne var + hugo;
+fim`
+
+    const sintatico = new Sintatico();
+    let arvoreSintatica = sintatico.parsear(sourceCode);
+
+    const semantico = new Semantico(arvoreSintatica!);
+    const arvoresDeExpressoes = semantico.validarComandos();
+    const tabelaDeSimbolos = semantico.tabelaDeSimbolos;
+
+    const intermediario = new Intermediario(arvoresDeExpressoes);
+    const gerados = intermediario.comandos;
+    const optimizados = intermediario.optimizar();
+
+    const mips = new Mips(tabelaDeSimbolos);
+    for (let i = 0; i < intermediario.totalComandos; ++i) {
+      gerados.push({
+        gerado: gerados[i],
+        otimizado: optimizados[i],
+        linha: arvoresDeExpressoes[i].extra!.linha,
+      } as any);
+
+      mips.adicionarInstrucoes(optimizados[i]);
+    }
+
+    const d3tree = getReactD3Tree(arvoreSintatica!);
+
+    expect(sintatico).toMatchSnapshot()
+    expect(arvoreSintatica).toMatchSnapshot()
+    expect(semantico).toMatchSnapshot()
+    expect(arvoresDeExpressoes).toMatchSnapshot()
+    expect(tabelaDeSimbolos).toMatchSnapshot()
+    expect(intermediario).toMatchSnapshot()
+    expect(gerados).toMatchSnapshot()
+    expect(optimizados).toMatchSnapshot()
+    expect(mips).toMatchSnapshot()
+    expect(d3tree).toMatchSnapshot()
+  })
+})
